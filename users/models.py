fe1b2a50d4577ba5fcd0834034eeacdd6f377c4c
avkucher <99530288+avkucher@users.noreply.github.com>
2022-08-06 23:02:55 +0300

    DZ_30

---
+++ users/models.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    lat = models.DecimalField(max_digits=8, decimal_places=6, null=True)
+    lng = models.DecimalField(max_digits=8, decimal_places=6, null=True)
+
+    class Meta:
+        verbose_name = "Местоположение"
+        verbose_name_plural = "Местоположения"
+
+    def __str__(self):
+        return self.name
+
+
+class User(AbstractUser):
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+
+    ROLES = [
+        (USER, USER),
+        (MODERATOR, MODERATOR),
+        (ADMIN, ADMIN),
+    ]
+
+    role = models.CharField(max_length=9, choices=ROLES, default=USER)
+    age = models.PositiveSmallIntegerField(null=True, blank=True)
+    locations = models.ManyToManyField(Location, blank=True)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ['username']
+
+    def __str__(self):
+        return self.username
